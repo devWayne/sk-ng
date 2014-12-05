2ca32950f9d0a925b7704a947a5bbade575c9d85
Wayne <dev.wayne.ix@gmail.com>
2014-12-05 19:54:02 +0800

    chgcss

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,6 +15,7 @@ skControllers.controller('skDealistCtrl', ['$scope', '$http', '$rootScope',
                 $rootScope.dealgroups = data.result.current[0].dealgroups;
                 $rootScope.nextdealgroups = data.result.next.dealgroups;
                 $rootScope.nexttime = data.result.next.time;
+                $scope.rich_buttons = data.result.rich_buttons;
                 $scope.now_deals = $rootScope.dealgroups;
                 $scope.next_deals = $rootScope.nextdealgroups;
             });
@@ -27,13 +28,15 @@ skControllers.controller('skDealistCtrl', ['$scope', '$http', '$rootScope',
 
 skControllers.controller('skDealCtrl', ['$scope', '$routeParams', '$rootScope', '$http', '$location', 'popupService',
     function($scope, $routeParams, $rootScope, $http, $location, popupService) {
-        //	$scope.status=$rootScope.status;
         $scope.dealId = $routeParams.dealId;
-        $scope.checkcode_flag = 0;
         $scope.checkcode_num = "";
         $scope.deal = {};
         $scope.toast = {};
 
+
+        /**
+         * deal init
+         */
         if ($rootScope.dealInfo[$scope.dealId] == undefined) {
             $http.jsonp('http://tgapp.51ping.com/qiang/ajax/nt/detail?city_id=' + $rootScope.cityid + '&dealgroup_id=' + $scope.dealId + '&callback=JSON_CALLBACK').success(function(data) {
                 $scope.deal.title = data.result.data.title;
@@ -48,7 +51,7 @@ skControllers.controller('skDealCtrl', ['$scope', '$routeParams', '$rootScope', 
                 $scope.deal.shop_count = data.result.shop_count;
                 $scope.deal.shops_url = data.result.shops_url;
                 $rootScope.dealInfo[$scope.dealId] = $scope.deal;
-                if (data.result.data.status != 0) {
+                if (data.result.data.status != 2) {
                     $scope.buy_words = "抢";
                     $scope.buy_class = "deal-buy-z";
                 } else {
@@ -56,12 +59,13 @@ skControllers.controller('skDealCtrl', ['$scope', '$routeParams', '$rootScope', 
                     $scope.buy_words = "抢光了";
                     $scope.buy_class = "deal-buy-o";
                     $rootScope.dealStatus[$scope.dealId] = 1;
+		    $scope.checkcode_open=0;
                 }
             });
         } else {
             $scope.deal = $rootScope.dealInfo[$scope.dealId];
             $http.jsonp('http://tgapp.51ping.com/qiang/ajax/nt/detail?city_id=' + $rootScope.cityid + '&dealgroup_id=' + $scope.dealId + '&callback=JSON_CALLBACK').success(function(data) {
-                if (data.result.data.status != 0) {
+                if (data.result.data.status != 2) {
                     $scope.buy_words = "抢";
                     $scope.buy_class = "deal-buy-z";
                 } else {
@@ -69,24 +73,33 @@ skControllers.controller('skDealCtrl', ['$scope', '$routeParams', '$rootScope', 
                     $scope.buy_words = "抢光了";
                     $scope.buy_class = "deal-buy-o";
                     $rootScope.dealStatus[$scope.dealId] = 1;
+		     $scope.checkcode_open=0;
                 }
             });
         }
 
-
+        /**
+         * listen checkcode length
+         * @param {varType} checkcode_num Description
+         * @param {varType} function Description
+         * @return {void} description
+         */
         $scope.$watch('checkcode_num', function() {
             if ($scope.checkcode_num.toString().length == 4) {
-                codecheck();
+                $scope.codecheck();
             }
         });
 
-        var codecheck = function() {
+        /**
+         * checkcode
+         * @return {void} description
+         */
+        $scope.codecheck = function() {
             $http.jsonp('http://tgapp.51ping.com/qiang/ajax/nt/verify-captcha?dealgroup_id=' + $scope.dealId + '&captcha=' + $scope.checkcode_num + '&dpid=' + $rootScope.dpid + '&version=' + $rootScope.version + '&city_id=' + $rootScope.cityid + '&callback=JSON_CALLBACK').success(function(data) {
                 if (data.code == 205 && data.result.advance_order_id) {
                     $scope.checkcode_close();
                     $scope.status = 2;
                     poll(data.result.advance_order_id);
-
                 }
                 if (data.code == 201) {
                     $scope.toast = popupService.openToast('提示', '请输入正确的验证码');
@@ -98,27 +111,28 @@ skControllers.controller('skDealCtrl', ['$scope', '$routeParams', '$rootScope', 
                     return;
 
                 } else {
-                    $scope.toast.title = '提示';
-                    $scope.toast.words = data.result.message;
-                    $scope.overlay_flag = 1;
-                    $scope.toast.words_flag = 1;
+                    $scope.toast = popupService.openToast('提示',data.result.message);
+		    $scope.checkcode_close();
                     setTimeout(function() {
-                        $scope.toast.words_flag = 0;
-                        $scope.overlay_flag = 0;
+                        $scope.toast = popupService.closeToast();
                         $scope.$apply();
-                        $scope.checkcode_open();
-                    }, 3000);
+		    }, 3000);
                     return;
-
                 }
             })
         };
 
+        /**
+         * poll check
+         * @param {varType} advance_order_id Description
+         * @return {void} description
+         */
         var poll = function(advance_order_id) {
             var poll_count = 0;
             var poll_timer = setInterval(function() {
                 $http.jsonp('http://tgapp.51ping.com/qiang/ajax/nt/poll?advance_order_id=' + advance_order_id + '&dealgroup_id=' + $scope.dealId + '&city_id=' + $rootScope.cityid + '&callback=JSON_CALLBACK').success(function(data) {
                     if (data.code == 200 && data.result.url) {
+                        $scope.checkcode_flag = 0;
                         clearInterval(poll_timer);
                         location.href = data.result.url;
                         return;
@@ -130,50 +144,63 @@ skControllers.controller('skDealCtrl', ['$scope', '$routeParams', '$rootScope', 
                         $scope.buy_class = "deal-buy-o";
                         $rootScope.dealStatus[$scope.dealId] = 1;
                         clearInterval(poll_timer);
-
+                        $scope.checkcode_flag = 0;
+			 $scope.checkcode_open=0;
                         return;
                     }
                     if (data.code == 205) {
+                        $scope.checkcode_flag = 0;
                         if (poll_count > 5) {
                             clearInterval(poll_timer);
                         }
                         poll_count++;
                         return;
-                    }
-                    if (data.code == 400) {
-                        alert('400:参数错误');
+                    } else {
+                        $scope.checkcode_flag = 0;
+                        $scope.status = 1;
+                        $scope.storage_flag = 1;
+                        $scope.buy_words = "抢光了";
+                        $scope.buy_class = "deal-buy-o";
+                        $rootScope.dealStatus[$scope.dealId] = 1;
+                        clearInterval(poll_timer);
+			$scope.checkcode_open=0;
                         return;
                     }
                 })
             }, 2000);
         };
 
+
+        /**
+         * view control
+         */
         $scope.storage_flag = 0;
-        $scope.openRemind = function() {
+        $scope.remind_open = function() {
             $scope.remind = popupService.openRemind();
         };
-        $scope.closeRemind = function() {
+        $scope.remind_close = function() {
             $scope.remind = popupService.closeRemind();
         }
         $scope.checkcode_open = function() {
             $scope.checkcode_num = "";
             $scope.checkcode_flag = 1;
             $http.jsonp('http://tgapp.51ping.com/qiang/ajax/nt/gen-captcha?dealgroup_id=' + $scope.dealId + '&city_id=' + $rootScope.cityid + '&callback=JSON_CALLBACK').success(function(data) {
-                    if (data.code == 203) {
-                        location.href = data.result.login_url;
-                        return;
-                    }
-                    if (data.code == 200) {
-                        $scope.checkcode_show = data.result.code;
-                    }
-                })
-                //$scope.overlay_flag = !$scope.overlay_flag;
+                if (data.code == 203) {
+		     $scope.checkcode_flag = 0;
+                    location.href = data.result.login_url;
+                    return;
+                }
+                if (data.code == 200) {
+                    $scope.checkcode_show = data.result.code;
+                }
+            })
         }
         $scope.checkcode_close = function() {
             $scope.checkcode_flag = 0;
         }
 
         /**
+         * remind check
          * @return {void} description
          */
         $scope.remindajax = function() {
@@ -191,30 +218,29 @@ skControllers.controller('skDealCtrl', ['$scope', '$routeParams', '$rootScope', 
             $http.jsonp('http://tgapp.51ping.com/qiang/ajax/nt/join?city_id=' + $rootScope.cityid + '&dealgroup_id=' + $scope.dealId + '&mobile=' + $scope.phone_num + '&callback=JSON_CALLBACK').success(function(data) {
 
                 if (data.code == 200) {
-                    $scope.toast.title = '设置成功';
-                    $scope.toast.words = '开抢前您将收到购买提醒';
+                    $scope.toast = popupService.openToast('设置成功', '开抢前您将收到购买提醒');
                 }
                 if (data.code == 201) {
-                    $scope.toast.title = '设置成功';
-                    $scope.toast.words = '您已经设置过提醒';
+
+                    $scope.toast = popupService.openToast('设置成功', '您已经设置过提醒');
+
                 }
                 if (data.code == 202) {
-                    $scope.toast.title = '设置失败';
-                    $scope.toast.words = '请供手机号码';
+                    $scope.toast = popupService.openToast('设置失败', '请供手机号码');
+
                 }
                 if (data.code == 203) {
+                    $scope.remind = popupService.closeRemind();
                     location.href = data.result.login_url;
+
                 }
                 if (data.code == 400) {
-                    $scope.toast.title = '设置失败';
-                    $scope.toast.words = '参数错误';
+                    $scope.toast = popupService.openToast('设置失败', '参数错误');
+
                 }
-                $scope.overlay_flag = 1;
-                $scope.toast.words_flag = 1;
-                $scope.remind_flag = 0;
+                $scope.remind = popupService.closeRemind();
                 setTimeout(function() {
-                    $scope.toast.words_flag = 0;
-                    $scope.overlay_flag = 0;
+                    $scope.toast = popupService.closeToast();
                     $scope.$apply();
                 }, 3000);
 
